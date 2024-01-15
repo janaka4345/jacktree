1284027c21cdb26fee345e5bd86a0795b1e1a8f9
banuka <banukaramanayaka76@gmail.com>
2024-01-15 15:27:32 +0530

    feat:hero section

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,8 +11,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className='' lang="en">
+      <body className={cn('relative h-full antialiased', inter.className)}>
+        <main className='relative flex flex-col min-h-screen flex-grow flex-1'>
+          {/* <div className='flex-grow flex-1'>{children}</div> */}{/*if footer wa added below uncomment*/}
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
 
 export default function Home() {
   return (
-    <div className='text-red-500 text-2xl'>hi jack tree</div>
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-secondary-foreground sm:text-6xl"><span className="text-primary">Dress to Impress</span> - Your Gateway to Unmatched Elegance and Comfort</h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground ">Welcome to JackTree – where style meets confidence. Explore our curated collection blending the latest trends with timeless elegance.  Your fashion evolution begins here</p>
+      </div>
+    </MaxWidthWrapper>
   )
 }
