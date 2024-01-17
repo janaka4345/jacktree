cac7f059f0e7c5b4c7fbddb9bdd514165ffd114c
janaka <129352300+janaka4345@users.noreply.github.com>
2024-01-17 05:49:26 +0000

    feat: Style changes  and shadeui/page.jsx, fix import and add new files and images to public directory

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
 import { NavItems } from "./NavItems";
+import { buttonVariants } from "./ui/button";
 
 export default function NavBar() {
+  const user=null
   return (
     <div className="sticky inset-x-0 top-0 z-50 h-16 bg-background">
       <header className="relative bg-background">
@@ -17,6 +19,57 @@ export default function NavBar() {
               </div>
               <div className="z-50 hidden lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
+              </div>
+              <div className='ml-auto flex items-center'>
+                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                  {user ? null : (
+                    <>
+                    <Link
+                      href='/sign-in'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Sign in
+                    </Link>
+                    <span
+                      className='h-6 w-px bg-border'
+                      aria-hidden='true'
+                    />
+                    </>
+
+                    
+                  )}
+                  {user ? (
+                    // <UserAccountNav user={user} />
+                    null
+                  ) : (
+                    <>
+                    <Link
+                      href='/sign-up'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Create account
+                    </Link>
+                    <span
+                      className='h-6 w-px bg-border'
+                      aria-hidden='true'
+                    />
+                    </>
+                  )}
+                  {/* {user ? null : (
+                    <div className='flex lg:ml-6'>
+                      <span
+                        className='h-6 w-px bg-border'
+                        aria-hidden='true'
+                      />
+                    </div> 
+                  )}*/}
+
+                  <div className='ml-4 flow-root lg:ml-6'>
+                    {/* <Cart /> */} cart
+                  </div>
+                </div>
               </div>
             </div>
           </div>
