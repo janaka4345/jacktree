c81c45a16cb1b3cb00c0fd9940c45b5548f73b68
banuka <banukaramanayaka76@gmail.com>
2024-03-07 17:36:04 +0530

    feat:log in with providers

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,12 @@
+import prisma from "@/lib/prismaClient"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 export const authOptions = {
     // Configure one or more authentication providers
+    adapter: PrismaAdapter(prisma),
+    session: { strategy: "jwt" },
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID,
